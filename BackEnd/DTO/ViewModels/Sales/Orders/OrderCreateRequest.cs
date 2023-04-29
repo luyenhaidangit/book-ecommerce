fa9d5adc93f7b80198650895a41ef4058dd8be40
luyenhaidangit <luyenhaidangit@gmail.com>
2023-04-29 17:01:28 +0700

    Create model order create request

---
+++ BackEnd/DTO/ViewModels/Sales/Orders/OrderCreateRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.ViewModels.Sales.Orders
+{
+    public class OrderCreateRequestOrderDetail
+    {
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+    public class OrderCreateRequest
+    {
+        public int CustomerId { get; set; }
+
+        public string Andress { get; set; }
+
+        public string Note { get; set; }
+
+        //public DateTime OrderDate { get; set; }
+
+        public int Status { get; set; }
+
+        public List<OrderCreateRequestOrderDetail> OrderDetails { get; set; }
+
+    }
+}
